424933912e8d6a3830890fb39364842bf68eede3
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2024-09-23 20:08:07 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    //Entrada:
+    int num, tabuada = 0;
+    printf("Digite um numero para fazer a tabuada: ");
+    scanf("%d", &num);
+//Processamento:
+for (tabuada = 0; tabuada<=10; tabuada++){
+        printf("%d x %d = %d\n", num, tabuada, num*tabuada);
+}
+return 0;
+}
